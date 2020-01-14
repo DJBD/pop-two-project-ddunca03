e02d2f9fca0f5cc09cb9fd37535ecc87e6fe0b2f
DJBD <57094924+DJBD@users.noreply.github.com>
2020-01-14 11:20:09 +0000

    writing some tests

--- Tests/fraction/FractionImplTest.java
+++ Tests/fraction/FractionImplTest.java
@@ -5,25 +5,38 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FractionImplTest {
+    Fraction third = new FractionImpl(1,3);
+    Fraction quarter = new FractionImpl(1,4);
+    Fraction half = new FractionImpl(1,2);
+    Fraction n_half = new FractionImpl(10,-20);
 
     @Test
-    void add() {
+    void add() throws Exception{
+        Fraction total = third.add(quarter);
+       assertEquals("7/12",total.toString());
     }
 
     @Test
-    void subtract() {
+    void subtract() throws Exception{
+        Fraction total = half.subtract(quarter);
+        assertEquals("1/4",total.toString());
+    }
+    @Test
+    void multiply() throws Exception{
+        Fraction total = half.multiply(half);
+        assertEquals("1/4",total.toString());
     }
 
     @Test
-    void multiply() {
+    void divide() throws Exception{
+        Fraction total = quarter.divide(half);
+        assertEquals("1/2",total.toString());
     }
 
     @Test
-    void divide() {
-    }
-
-    @Test
-    void abs() {
+    void abs() throws Exception{
+        Fraction total = n_half.abs();
+        assertEquals("1/2",total.toString());
     }
 
     @Test
--- src/fraction/Main.java
+++ src/fraction/Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Fraction a = new FractionImpl(1,3);
-        Fraction b = new FractionImpl(1,3);
+        Fraction b = new FractionImpl(1,4);
         Fraction c = a.add(b);
 
         System.out.println(c.toString());
-        System.out.println(c.getClass());
-
     }
 }
