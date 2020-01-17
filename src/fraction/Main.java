e469b001bd729514a0773a5b424f87a04aea3ccd
DJBD <57094924+DJBD@users.noreply.github.com>
2020-01-17 13:53:32 +0000

    Simple Fraction Calculator Program

--- src/fraction/Main.java
+++ src/fraction/Main.java
@@ -16,18 +16,42 @@ public class Main {
 
         boolean done = true;
         while (done){
-            System.out.println("Would you like to do something else?");
-            Scanner yesorno = new Scanner(System.in);
-            String nextInp = yesorno.nextLine();
-            System.out.println(nextInp);
-            if (nextInp == "yes"){
-                System.out.println("Please enter: x / + or -");
+            System.out.println("Would you like to do something else? yes or no");
+            Scanner yes = new Scanner(System.in);
+            String nextInp = yes.nextLine();
+            if (nextInp.equals("yes") || nextInp.equals("y")){
+                System.out.println("Please enter: \"x\" for (times), \"/\" for (divide), \"+\" for (add) or \"-\" for (subtract)");
+                Scanner sign = new Scanner(System.in);
+                String signInp = sign.nextLine();
+                System.out.println("Please enter another Fraction: ");
+                Scanner secondFraction = new Scanner(System.in);
+                var userInput2 = secondFraction.nextLine();
+                Fraction secondFrac = new FractionImpl(userInput2);
+                if (signInp.equals("x")) {
+                    Fraction total = firstFrac.multiply(secondFrac);
+                    System.out.println(firstFrac.toString() + " " + signInp + " " + secondFrac.toString() + " = " + total.toString());
+                    firstFrac = total;
+                }
+                if (signInp.equals("/")) {
+                    Fraction total = firstFrac.divide(secondFrac);
+                    System.out.println(firstFrac.toString() + " " + signInp + " " + secondFrac.toString() + " = " + total.toString());
+                    firstFrac = total;
+                }
+                if (signInp.equals("+")) {
+                    Fraction total = firstFrac.add(secondFrac);
+                    System.out.println(firstFrac.toString() + " " + signInp + " " + secondFrac.toString() + " = " + total.toString());
+                    firstFrac = total;
+                }
+                if (signInp.equals("-")) {
+                    Fraction total = firstFrac.subtract(secondFrac);
+                    System.out.println(firstFrac.toString() + " " + signInp + " " + secondFrac.toString() + " = " + total.toString());
+                    firstFrac = total;
+                }
             }
-            else done = false;
+            else {
+                System.out.println("Thank you for using this FractionProgram");
+                done = false;
+            }
         }
-
-
-
-
     }
 }
