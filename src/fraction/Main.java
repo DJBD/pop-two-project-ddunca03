105597d41745dd6fb03b32b6023317b45025d2e8
DJBD <57094924+DJBD@users.noreply.github.com>
2020-01-17 13:11:40 +0000

    main program

--- src/fraction/Main.java
+++ src/fraction/Main.java
@@ -6,13 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to your FractionProgram!" + "\n" + "You can use the following features: add, subtract, multiply, divide, absolute, inverse, equal to, compare to");
-
+        System.out.println("Welcome to your FractionProgram!");
+        System.out.println("Please enter a Fraction (n/d): ");
         Scanner firstFraction = new Scanner(System.in);
         var userInput = firstFraction.nextLine();
-
+        System.out.print("You have entered: ");
         Fraction firstFrac = new FractionImpl(userInput);
         System.out.println(firstFrac.toString());
+
+        boolean done = true;
+        while (done){
+            System.out.println("Would you like to do something else?");
+            Scanner yesorno = new Scanner(System.in);
+            String nextInp = yesorno.nextLine();
+            System.out.println(nextInp);
+            if (nextInp == "yes"){
+                System.out.println("Please enter: x / + or -");
+            }
+            else done = false;
+        }
 
 
 
